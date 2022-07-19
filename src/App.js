b63e4818ba28852c69238357cc08ef5349da8e0f
Nukukoricchio <cuongpintu219@gmail.com>
2022-07-20 04:26:43 +0700

    todoapp React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Traing yolov5 2d detection model",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Traing yolov5 2d detection model",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Traing yolov5 2d detection model",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "Traing yolov5 2d detection model",
+      time: "20/07/2022 19:30",
+      reminder: true,
+    },
+  ]);
+
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="container">
+      <Header title="Hello Cuongnq" />
+      <Routes>
+        <Route path='/' element={
+          <>
+           <AddTask onAdd={addTask} />
+            {Tasks.length > 0 ? (
+              <Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleReminder}
+              />
+            ) : (
+              "No tasks found"
+            )}
+            <Link to="/about">About page</Link>
+          </>}>
+        </Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2022</p>
+      <Link to='/'>Home</Link>
+    </footer>
+  )
+}
+
+export default About
