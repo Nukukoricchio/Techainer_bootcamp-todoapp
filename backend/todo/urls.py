176a761233e709e1eb257b6246b901e3f7e461eb
Nukukoricchio <cuongpintu219@gmail.com>
2022-08-09 10:28:02 +0700

    push backend

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.TodoListView.as_views()),
-    path('<int:pk>/', views.TodoDetailView.as_views())
+    path('', views.TodoListView.as_view()),
+    path('<int:pk>/', views.TodoDetailView.as_view())
 ]
